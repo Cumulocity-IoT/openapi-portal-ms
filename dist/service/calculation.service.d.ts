cc33b3dac9c65509222e98847f02c8d07178d83c
Hendrik Näther <hendrik.naether@cumulocity.com>
2025-08-25 14:48:03 +0200

    3rd commit

--- dist/service/calculation.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { APIService } from './api.service';
-import { IManagedObject } from '@c8y/client';
-export declare class CalculationService {
-    private api;
-    private readonly logger;
-    constructor(api: APIService);
-    calculateLatestValue(device: IManagedObject, eligibleDatapoints: string[]): Promise<void>;
-    calculateAggregates(device: IManagedObject, eligibleDatapoints: string[]): Promise<void>;
-}
--- dist/service/microservice-settings.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { APIService } from './api.service';
-export declare class KOCSettingsService {
-    private api;
-    constructor(api: APIService);
-    private fetchOrFallbackTo;
-    private createDefault;
-}
