5886a8f61a9b52a8cd2167eb64da8f54a702b467
Hendrik Näther <hendrik.naether@cumulocity.com>
2025-08-28 09:50:16 +0200

    Added logging for session and custom events.

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,16 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { GainsightPxService } from './service/gainsight-px.service';
-import { CustomEvent } from './model/gainsight-px.model';
+import { CustomEvent, CustomEventFilter } from './model/gainsight-px.model';
+import { subDays } from 'date-fns';
 
 @Controller()
 export class EventsController {
+  readonly logger = new Logger(EventsController.name);
+
   constructor(private api: GainsightPxService) {}
 
-  @Get('/customEvents')
-  async getCustomEvents() {
+  @Get('/customEventsToday')
+  async getCustomEventsToday() {
     // @ts-ignore
-    const customEvents = await this.api.getCustomEvents({ filter: 'accountId~t2700*', sort: 'date', pageSize: 1000 });
+    // always returns latest date first, only returns for today
+    const filter = 'accountId~t2700*;globalContext.projectName===devicemanagement' as CustomEventFilter;
+    this.logger.log(`Fetching custom events for today: ${filter}`);
+    const customEvents = await this.api.getCustomEvents({ filter, sort: 'date', pageSize: 1000 });
+    this.logger.log(`Fetched ${customEvents.customEvents.length} custom events.`);
     return customEvents;
+  }
+
+  @Get('/customEventsLastMonth')
+  async getCustomEventsLastMonth() {
+    // @ts-ignore
+    // always returns latest date first, only returns for today
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+    const last30Days = subDays(date, 30);
+
+    const filter = `accountId~t2700*;date>${last30Days.getTime()};globalContext.projectName===devicemanagement` as CustomEventFilter;
+    this.logger.log(`Fetching custom events for today: ${filter} (date: ${last30Days.toISOString()})`);
+    const customEvents = await this.api.getCustomEvents({ filter, sort: 'date', pageSize: 1000 });
+
+    if (!customEvents.customEvents || customEvents.customEvents.length === 0) {
+      this.logger.log('No events found for the last 30 days.');
+      return [];
+    } else if (customEvents.customEvents.length < 1000) {
+      this.logger.log(`Received ${customEvents.customEvents.length} events. No pagination needed.`);
+      return customEvents.customEvents;
+    } else {
+      this.logger.log('More than 1000 custom events found, fetching with pagination...');
+      const allEvents = await this.getEventWithPagination(customEvents.scrollId, customEvents.customEvents, filter);
+      return allEvents;
+    }
+  }
+
+  private async getEventWithPagination(scrollId: string, sum: CustomEvent[], filter: CustomEventFilter): Promise<CustomEvent[]> {
+    const res = await this.api.getCustomEvents({
+      filter,
+      sort: 'date',
+      pageSize: 1000,
+      scrollId,
+    });
+
+    sum.push(...res.customEvents);
+    this.logger.log(`Pagination - received ${res.customEvents.length} custom events - overall count ${sum.length} events.`);
+    if (res.customEvents.length < 1000) {
+      return sum;
+    } else {
+      return this.getEventWithPagination(res.scrollId, sum, filter);
+    }
   }
 
   @Get('/widgetEvents')
--- src/session-events.controller.ts
+++ src/session-events.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { GainsightPxService } from './service/gainsight-px.service';
-import { UserUtilityService } from './service/user-utility.service';
 import { subDays } from 'date-fns';
 import { SessionEvent, SessionEventFilter } from './model/gainsight-px.model';
 
 @Controller()
 export class SessionEventsController {
+  private readonly logger = new Logger(SessionEventsController.name);
+
   constructor(private api: GainsightPxService) {}
 
   @Get('/sessionEventsLastMonth')
@@ -15,6 +16,8 @@ export class SessionEventsController {
     const thirtyDaysAgo = subDays(startDate, 30);
     const filter = `accountId~t2700*;date>${thirtyDaysAgo.getTime()}` as SessionEventFilter;
 
+    this.logger.log(`Fetching session events with filter: ${filter} (${thirtyDaysAgo.toISOString()})`);
+
     const res = await this.api.getSessionEvents({ filter, sort: 'date', pageSize: 1000 });
     const events = res.sessionInitializedEvents;
     if (!events || events.length === 0) {
@@ -22,6 +25,7 @@ export class SessionEventsController {
     }
 
     if (events.length < 1000) {
+      this.logger.log(`Received ${events.length} events. No pagination needed.`);
       return this.aggregateByDay(events);
     }
 
@@ -46,6 +50,31 @@ export class SessionEventsController {
       .sort((a, b) => (a.date < b.date ? -1 : 1));
   }
 
+  @Get('/sessionEventsLastDay')
+  async getSessionEventsToday() {
+    const startDate = new Date();
+    const twentyFourHoursAgo = subDays(startDate, 1);
+    const dateStamp = twentyFourHoursAgo.getTime();
+
+    const filter = `accountId~t2700*;date>${dateStamp}` as SessionEventFilter;
+
+    this.logger.log(`Fetching session events with filter: ${filter} (${twentyFourHoursAgo.toISOString()})`);
+
+    const res = await this.api.getSessionEvents({ filter: filter, sort: 'date', pageSize: 1000 });
+    const events = res.sessionInitializedEvents;
+    if (!events || events.length === 0) {
+      return [];
+    }
+
+    if (events.length < 1000) {
+      this.logger.log(`Received ${events.length} events. No pagination needed.`);
+      return this.aggregateByMinute(events);
+    }
+
+    const allEvents = await this.getSessionEventsWithPagination(res.scrollId, events, filter);
+    return this.aggregateByMinute(allEvents);
+  }
+
   private async getSessionEventsWithPagination(scrollId: string, sum: SessionEvent[], filter: SessionEventFilter) {
     const res = await this.api.getSessionEvents({
       filter,
@@ -56,31 +85,12 @@ export class SessionEventsController {
 
     const events = res.sessionInitializedEvents;
     sum.push(...events);
+    this.logger.log(`Pagination - received ${events.length} events - overall count ${sum.length} events.`);
     if (events.length < 1000) {
       return sum;
     } else {
       return this.getSessionEventsWithPagination(res.scrollId, sum, filter);
     }
-  }
-  @Get('/sessionEventsLastDay')
-  async getSessionEventsToday() {
-    const startDate = new Date();
-    const twentyFourHoursAgo = subDays(startDate, 1);
-    const dateStamp = twentyFourHoursAgo.getTime();
-
-    const filter = `accountId~t2700*;date>${dateStamp}` as SessionEventFilter;
-    const res = await this.api.getSessionEvents({ filter: filter, sort: 'date', pageSize: 1000 });
-    const events = res.sessionInitializedEvents;
-    if (!events || events.length === 0) {
-      return [];
-    }
-
-    if (events.length < 1000) {
-      return this.aggregateByMinute(events);
-    }
-
-    const allEvents = await this.getSessionEventsWithPagination(res.scrollId, events, filter);
-    return this.aggregateByMinute(allEvents);
   }
 
   private aggregateByMinute(events: SessionEvent[]) {
