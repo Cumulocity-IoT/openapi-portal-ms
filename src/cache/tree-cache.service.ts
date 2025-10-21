4804378e59e19ca7aafa2a0cb3e7786d76677a09
Hendrik Näther <hendrik.naether@cumulocity.com>
2025-10-21 10:42:44 +0200

    Added better logging and checks

--- src/cache/tree-cache.service.ts
+++ src/cache/tree-cache.service.ts
@@ -11,7 +11,8 @@ export abstract class TreeCache<T> {
   newest: T;
 
   setCache(items: T[]) {
-    if (items.length === 0) {
+    const length = items.length;
+    if (length === 0) {
       this.tree = new IntervalTree<GenericInterval<T>>();
       return;
     }
@@ -24,23 +25,34 @@ export abstract class TreeCache<T> {
     }
     this.tree = tree;
 
+    // Verify data is sorted in ascending order by date
+    for (let i = 1; i < length; i++) {
+      if (this.getDate(items[i]) < this.getDate(items[i - 1])) {
+        this.getLogger().error('Cache items must be sorted by date in ascending order!');
+      }
+    }
+
     this.oldest = items[0];
-    this.newest = items[items.length - 1];
+    this.newest = items[length - 1];
+    if (items.length > 2 && this.getDate(this.oldest) > this.getDate(this.newest)) {
+      this.getLogger().error(`Cache items must be sorted by date in ascending order, but oldest is newer than newest! ${this.getDate(this.oldest)} > ${this.getDate(this.newest)}`);
+    }
   }
 
   getCache(start: number, end: number): T[] {
     // Check if range is completely outside cache bounds
     if (start > this.getDate(this.newest)) {
-      this.getLogger().log('Start date param is more recent than the newest lastSeenDate in the cache.');
+      this.getLogger().log(`Start date param is more recent than the newest lastSeenDate in the cache. ${start} > ${this.getDate(this.newest)}`);
       return [];
     }
 
     if (end < this.getDate(this.oldest)) {
-      this.getLogger().log('End date param is older than the oldest lastSeenDate in the cache.');
+      this.getLogger().log(`End date param is older than the oldest lastSeenDate in the cache. ${end} < ${this.getDate(this.oldest)}`);
       return [];
     }
 
     const results = this.tree.search(start, end);
+    this.getLogger().log(`Cache query from ${new Date(start).toISOString()} to ${new Date(end).toISOString()} returned ${results.length} items.`);
     return results.map((interval) => interval.item);
   }
 
